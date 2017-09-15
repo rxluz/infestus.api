365574ac58813a1ac4ea3309ad210aa1192999e7
rxluz <rx@appock.co>
2017-09-15 21:36:21 +0100

    express install

--- app.js
+++ app.js
@@ -1,23 +1,6 @@
-// Reuire diet
-var server = require('diet');
+const express = require('express')
+const app = express()
 
-// Main domain
-var app = server()
-app.listen('https://api.infestus.cc/')
-app.get('/', function($){
-    $.end('hello world api.infestus.cc ')
-})
-
-// Sub domain
-var sub = server()
-sub.listen('https://apinew.infestus.cc/')
-sub.get('/', function($){
-    $.end('hello world at apinew!')
-})
-
-// Other domain
-var other = server()
-other.listen('http://other.com/')
-other.get('/', function($){
-    $.end('hello world at other domain')
+app.get('/', (req, res) => {
+  res.send('Hello World!')
 })
--- config/dbConnection.js
+++
@@ -1,6 +0,0 @@
-module.exports =  {
-
-	getMongoConnection: function() {
-		return "mongodb://infestususer:J_7./'7?!x0s9&q@ds127854.mlab.com:27854/infestus";
-	}
-}
